97b51cdcd21256cf4308d419f4561f1896d1626e
Kutub Udd <socialnetwork98710@gmail.com>
2024-03-07 19:53:50 +0600

    added

--- src/UI/Login.js
+++ src/UI/Login.js
@@ -31,7 +31,7 @@ const LoginCard = () => {
             className="mx-1 h-8 w-8 cursor-pointer"
             onClick={() =>
               signIn('google', {
-                callbackUrl: 'ttps://hafejia-travel-tours.vercel.app/sidebar',
+                callbackUrl: 'https://hafejia-travel-tours.vercel.app/sidebar',
               })
             }
           />
